351e1e138f1c4c42b606da4f27305d5516db7810
Kimmo Parviainen-Jalanko <kimvais@kimva.is>
2020-12-01 09:01:01 +0200

    Day 1, part 1

---
+++ Program.fs
@@ -0,0 +1,19 @@
+﻿open System.IO
+let readLines filePath = File.ReadLines(filePath)
+
+let readInput (s: string) =
+    readLines (__SOURCE_DIRECTORY__ + (sprintf "/input/%s.txt" s))
+let getProblem (a: seq<string>): string = a |> Seq.head
+
+
+let day1 () =
+    let entries = readInput "1" |> Seq.map int32 |> Seq.cache
+    Seq.allPairs entries entries |> Seq.filter (fun (a,b) -> (a <> b) && (a + b = 2020)) |> Seq.head |> (fun (a,b) -> a * b)
+    
+    
+[<EntryPoint>]
+let main argv =
+    let day = argv |> getProblem
+    match day with
+    | "1" -> day1 ()
+    | _ -> -1
