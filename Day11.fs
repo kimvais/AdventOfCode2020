f6f4351583ca0354d83f7f60684d643f8b4036bc
Kimmo Parviainen-Jalanko <kimvais@kimva.is>
2020-12-11 09:20:22 +0200

    Day 11, WIP
    ... it was a mistake to concatenate the rows :)

--- Day11.fs
+++ Day11.fs
@@ -6,58 +6,64 @@ open AoC2020.Utils
 let printSeats seats =
     for row in seats do
         printfn "%s" (row |> Array.map string |> String.concat "")
-let findNeighbours offset (seats: char array) pos =
-        let indices =
-            [ pos - offset - 1
-              pos - offset
-              pos - offset + 1
-              pos - 1
-              pos + 1
-              pos + offset - 1
-              pos + offset
-              pos + offset + 1 ]
+let findNeighbours (seats: char [] []) x y =
+        let numRows = Array.length seats
+        let numCols = Array.length seats.[0]
+        let areValid x y =
+            x >= 0 && y >= 0 && x < numRows && y < numCols
+        [ x - 1, y - 1
+          x - 1, y
+          x - 1, y
+          x, y - 1
+          x, y + 1
+          x + 1, y - 1
+          x + 1, y 
+          x + 1, y + 1 ]
 
-        indices
-        |> Seq.filter (fun i -> (i >= 0) && (i < Seq.length seats - 1))
-        |> Seq.map (fun i -> seats.[i])
+        |> Seq.filter (fun (x, y) -> areValid x y )
+        |> Seq.map (fun (x,y) -> seats.[x].[y])
 
-let countNeighbours offset seats pos =
-    findNeighbours offset (seats: char array) pos
+let countNeighbours seats x y =
+    findNeighbours seats x y
     |> Seq.map (function
         | 'L'
         | '.' -> 0
         | '#' -> 1)
     |> Seq.sum
 
-let reseat offset (seats: char array) =
-    let getNewState i =
-        match seats.[i] with
+let reseat (seats: char [][]) =
+    let getNewState x y =
+        match seats.[x].[y] with
         | '.' -> '.'
         | _ ->
-            match countNeighbours offset seats i with
+            match countNeighbours seats x y with
             | n when n < 4 -> '#'
             | _ -> 'L'
-
-    { 0 .. (Seq.length seats - 1)}
-    |> Seq.map getNewState
+    seq {
+        for row in { 0 .. (Seq.length seats - 1)} do
+            yield seq {
+                for col in {0 .. (Seq.length seats.[0] - 1)} do
+                    yield getNewState row col
+            } |> Array.ofSeq
+    }
     |> Array.ofSeq
     
 let day11 fn () =
-    let floorPlan =
+    let allSeats =
         readInput fn
         |> Seq.map (Array.ofSeq)
         |> Array.ofSeq
 
-    printSeats floorPlan
-    let offset = floorPlan |> Array.head |> Array.length
-    let allSeats = floorPlan |> Array.concat
+    printSeats allSeats
 
     let unfolder state =
         printfn ""
-        let newState = reseat offset state
+        let newState = reseat state
         if newState = state then
             None
         else
-            newState |> Seq.chunkBySize offset |> printSeats
+            newState |> printSeats
             Some (newState, newState)
-    Seq.unfold unfolder allSeats |> Seq.last |> Array.filter (fun c -> c = '#') |> Array.length |> int64
+    let seatingProgress = Seq.unfold unfolder allSeats
+    seatingProgress |> Seq.take 5 |> Seq.iter (fun _ -> ())
+    seatingProgress|> Seq.head |> Array.concat |> Array.filter (fun c -> c = '#') |> Array.length |> int64
