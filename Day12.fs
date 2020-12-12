bfcc7fcc894ee565042cd59034d7ce5bfa5302ce
Kimmo Parviainen-Jalanko <kimvais@kimva.is>
2020-12-12 12:17:18 +0200

    Day 12 part 2 WIP.

--- Day12.fs
+++ Day12.fs
@@ -8,9 +8,7 @@ type Direction =
     | South
     | West
 
-type Movement =
-    | Forward
-    | Backward
+type Movement = | Forward
 
 type Turn =
     | Left
@@ -18,6 +16,9 @@ type Turn =
 
 
 type Ship = { X: int; Y: int; Facing: Direction }
+type Vector = { X: int; Y: int }
+
+type VectoredShip = { X: int; Y: int; Waypoint: Vector }
 
 let turnRight =
     function
@@ -34,50 +35,60 @@ let turnLeft =
     | West -> South
 
 let turnShip dir degrees ship =
-    let times = degrees / 90 
+    let times = degrees / 90
 
     let turnFn =
         match dir with
-        | Left ->
-            turnLeft
-        | Right ->
-            turnRight
+        | Left -> turnLeft
+        | Right -> turnRight
 
     let folder f _ = turnFn f
 
     let newFacing =
-        [ 0 .. times - 1] |> Seq.fold folder ship.Facing
+        [ 0 .. times - 1 ] |> Seq.fold folder ship.Facing
 
     { ship with Facing = newFacing }
 
-let move (direction: Direction option) distance ship =
-    let dir =
-        match direction with
-        | Some d -> d
-        | None -> ship.Facing
 
+let move dir distance (ship: Ship) =
     match dir with
     | North -> { ship with X = ship.X - distance }
     | East -> { ship with Y = ship.Y + distance }
     | South -> { ship with X = ship.X + distance }
     | West -> { ship with Y = ship.Y - distance }
 
+let moveShip (direction: Direction option) distance ship =
+    let dir =
+        match direction with
+        | Some d -> d
+        | None -> ship.Facing
+
+    move dir distance ship
+
 let parseInstruction (s: string) =
     let arg = int s.[1..(String.length s - 1)]
     match s.[0] with
-    | 'N' -> move (Some North) arg
-    | 'E' -> move (Some East) arg
-    | 'S' -> move (Some South) arg
-    | 'W' -> move (Some West) arg
+    | 'N' -> moveShip (Some North) arg
+    | 'E' -> moveShip (Some East) arg
+    | 'S' -> moveShip (Some South) arg
+    | 'W' -> moveShip (Some West) arg
     | 'L' -> turnShip Left arg
     | 'R' -> turnShip Right arg
-    | 'F' -> move None arg
+    | 'F' -> moveShip None arg
 
 let day12 fn () =
-    let mutable ship = { X = 0; Y = 0; Facing = East }
-    let folder s fn =
-        fn s
+    let ship = { X = 0; Y = 0; Facing = East }
+    let folder s fn = fn s
     let instructions = readInput fn |> Seq.map parseInstruction
     instructions
     |> Seq.fold folder ship
-    |> (fun s -> abs s.X + abs s.Y) |> int64
+    |> (fun s -> abs s.X + abs s.Y)
+    |> int64
+
+let day12part2 fn () =
+    let ship =
+        { X = 0
+          Y = 0
+          Waypoint = { X = -1; Y = 10 } }
+
+    ship.X
