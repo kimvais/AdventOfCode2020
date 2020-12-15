a747484f2f3172c8a4b45782dbc03330b485c640
Kimmo Parviainen-Jalanko <kimvais@kimva.is>
2020-12-15 15:02:14 +0200

    Rewrote day 15 for part 2.

--- Day15.fs
+++ Day15.fs
@@ -1,33 +1,29 @@
 module AoC2020.Day15
 
-open AoC2020.Utils
-
-type State = { Numbers: int list
-               Previous: int }
-
-let generateNumber state =
-    let previous =
-        match state.Numbers with
-        | head :: tail ->
-            match tail |> List.tryFindIndex ((=) head) with
-            | Some i -> i + 1
+let rec play round previous roundsToPlay seen =
+    if round % 1_000_000 = 0 then
+        printfn "Round: %d" round
+    match round = roundsToPlay with
+    | true -> previous
+    | false ->
+        let newNumber =
+            match seen |> Map.tryFind previous with
             | None -> 0
+            | Some n -> (round - 1) - n
 
-    { state with
-          Previous = previous
-          Numbers = [ previous ] @ state.Numbers }
+        play (round + 1) newNumber roundsToPlay (seen |> Map.add previous (round - 1))
+
 
 let day15 (input: string) n () =
     let numbers =
         input.Split(",")
         |> List.ofSeq
         |> List.map int
-        |> List.rev
 
-    let count = n - List.length numbers
-    let mutable state = { Numbers = numbers; Previous = List.head numbers }
-    for i in [ 1 .. count ] do
-        if i % 1000 = 0 then printfn "%d" i
-        state <- generateNumber state
+    let seen =
+        numbers
+        |> List.mapi (fun i e -> (e, i + 1))
+        |> Map.ofList
 
-    state.Previous |> int64
+    play (List.length numbers + 1) (List.last numbers) (n + 1) seen
+    |> int64
